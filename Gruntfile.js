155fd2be9c5e3bbcd5d380ebbee07748428df797
Michael Henning <myklhenn@icloud.com>
2016-07-20 16:21:01 -0700

    Project init w/folders, npm, grunt, etc.

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+  // project configuration
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          keepalive: true,
+          open: {
+            target: 'http://localhost:9001',
+            appName: 'open'
+          }
+          // (specify root directory option here if needed)
+        }
+      }
+    },
+    link_html: {
+      build: {
+        jsFiles: [
+          '**/angular.min.js',
+          '**/angular-route.min.js',
+          // '**/angular-animate.min.js',
+          '**/app.js'
+        ],
+        cssFiles: [
+          // 'content/css/styles.css'
+        ],
+        targetHtml: ['index.html']
+      }
+    }
+  });
+
+  // load plugins
+  grunt.loadNpmTasks('grunt-link-html');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  // add tasks
+  grunt.registerTask('default', ['connect']);
+  grunt.registerTask('link', ['link_html']);
+
+};
